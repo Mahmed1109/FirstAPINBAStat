58c5e4d163ccf7d934729e5d1c4c0fd60dc5af18
Mahmed1109 <m.ahmed110900@gmail.com>
2025-04-11 11:43:03 +0100

    Program essentially finished formating is off so need to work on that

--- nbastats.py
+++ nbastats.py
@@ -1,19 +1,46 @@
-from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.endpoints import PlayerGameLog
 #playergamelog gives you game stats for player, points, assists, rebounds for each game.
 from nba_api.stats.static import players
 #imports all players current and past
 import pandas as pd
-#used to display stats in tables from the info i get from API
+#used to create data table
+from tabulate import tabulate
+#takes the dataframe table and makes it into a readable format
 
-def search_player(name):
+def search_player(pname):
     # Function to search for player if not found it will return msg Player not found 
-    player = players.find_players_by_full_name(name)
+    player = players.find_players_by_full_name(pname)
     if not player:
         return "Player cant be found"
     return player[0]
 
 def get_stats(player_id):
     # Function to grab the games for the season specified for this instance is the 24/25 season
-    gamelog = playergamelog(player_id=player_id, season='2024-25')
+    gamelog = PlayerGameLog(player_id=player_id, season='2024-25')
     df = gamelog.get_data_frames()[0]
+    
+
+def main():
+    pname=input("Enter the players full name please:")
+
+    player=search_player(pname)
+    #checks for player is a viable string in the season search
+    #isinstance is python function, first parameter is player input and second is checking if its a string
+    if isinstance(player,str):
+        print(player)
+        return
+    
+    try:
+        stats=get_stats(player['id'])
+        print(f"\n Stats for {player['full_name']})(Season 24-25):")
+        print(tabulate(stats,headers='keys', tablefmt='grid', showindex=False))
+        #tablemft=grid is just the style, and showindex just turns the row numbers off
+
+    except Exception as e:
+        print(f"Error getting data: {e}")
+
+if __name__=="__main__":
+    main()
+
+
 
